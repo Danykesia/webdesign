8a66d52c6660f1ffadbfc07bd37c4e5e4be76252
Pedro de Almeida <pedro_almeida1191@hotmail.com>
2024-05-05 14:36:10 +0100

    chore: update vite config

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,11 @@ export default {
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
+        mainPt: resolve(__dirname, 'index-pt.html'),
+        about: resolve(__dirname, 'about.html'),
+        aboutPt: resolve(__dirname, 'sobre.html'),
+        work: resolve(__dirname, 'work.html'),
+        workPt: resolve(__dirname, 'trabalhos.html'),
       }
     }
   }
